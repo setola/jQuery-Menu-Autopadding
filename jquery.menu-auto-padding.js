d9ff8708f54630470e6a47eec275bc19a0850591
setola@gmail.com <setola@gmail.com@7041f923-b79d-2ef1-923e-760155073311>
2011-04-21 13:41:11 +0000

    added a new way to space the elements: everyone should be position:absolute; left:0; top:0; and then the script modifies the width of every object. Pay attenction to the z-index of every element.
    added option type
    added option separatorWidth

--- jquery.menu-auto-padding.js
+++ jquery.menu-auto-padding.js
@@ -1,16 +1,27 @@
 (function($){
 	/**
-	 * this script automatically changes the padding of an element
+	 * this script automatically changes the spacing of an element
 	 * according to the maximum width of the parent divided by
-	 * the number of children. Tipically used to center a
-	 * dynamic orizontal menu.
+	 * the number of children. Tipically used to dynamically
+	 * center an orizontal menu.
 	 */
   $.fn.autopadding = function(options) {
 		var settings = {
-				/**
-				 * jquery selector of the elements to be padded
-				 */
-			elementsSelector		: '.autopadding'
+			/**
+			 * jquery selector of the elements to be padded
+			 */
+			elementsSelector		: '.autopadding',
+			/**
+			 * the way the menu is builded:
+			 * padding = applyes a padding-right:x and padding-left:x to every element
+			 * width left = apply a dynamic width to every element
+			 */
+			type								:	'padding',
+			/**
+			 * if you need some king of graphic separator between every element
+			 * you have to insert his width here
+			 */
+			separatorWidth			:	0
 		};
 		if(options) $.extend(settings,options);
 		
@@ -21,13 +32,48 @@
 			
 			elements.each(function(){
 				//elementsWidth += $(this).width();
-				elementsWidth += $(this).outerWidth();
+				elementsWidth += $(this).outerWidth(true);
 			});
-			var padding = Math.floor(((($(this).outerWidth())-elementsWidth)/elements.size())/2);
 			
-			elements.each(function(){
-				$(this).css({'padding-right': padding, 'padding-left': padding});
-			});
+			var css = {};
+			
+			switch(settings.type) {
+				case 'padding':
+					var padding = Math.floor(((($(this).outerWidth(true))-elementsWidth)/elements.size())/2);
+					elements.each(function(){
+						$(this).css({
+							'padding-right'		:		padding, 
+							'padding-left'		:		padding
+						});
+					});
+				break;
+				
+				//TODO: need some test here
+				case 'left':
+					var delta = Math.floor(((($(this).outerWidth(true))-elementsWidth)/elements.size()));
+					var pointer = 0;
+					elements.each(function(index){
+						$(this).css({
+							'left'		:		pointer
+						});
+						pointer = pointer+delta+$(this).outerWidth(true);
+					});
+				break;
+					
+				case 'width left':
+					var delta = Math.floor(((($(this).outerWidth(true))-elementsWidth)/elements.size()))-Math.floor(settings.separatorWidth/elements.size());
+					var pointer = 0;
+					elements.each(function(index){
+						pointer = pointer+$(this).outerWidth(true);
+						$(this).css({
+							'width'		:		pointer
+						});
+						pointer = pointer+delta;
+					});
+				break;
+			}
+			
+			
 		});
 	};  
 })(jQuery); 
