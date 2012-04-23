c085694d40ec81c79b07cc31a351d5ce1003bb25
setola@gmail.com <setola@gmail.com@7041f923-b79d-2ef1-923e-760155073311>
2012-04-23 11:39:12 +0000

    Added callback on every element rendering and on every instance finished.

--- jquery.menu-auto-padding.js
+++ jquery.menu-auto-padding.js
@@ -4,10 +4,10 @@
 	 * according to the maximum width of the parent divided by
 	 * the number of children. Tipically used to dynamically
 	 * center an orizontal menu.
-	 * @version 1.0_r9
+	 * @version 1.1
 	 */
   $.fn.autopadding = function(options) {
-		var settings = {
+		this.settings = {
 			/**
 			 * jquery selector of the elements to be padded
 			 */
@@ -26,60 +26,97 @@
 			/**
 			 * space of the container wich will not be calculated as free space
 			 */
-			containerBorder			:	0
+			containerBorder			:	0,
+			/**
+			 * this is called on every element when all the math stuff is done 
+			 * and we know how many padding add to every element.
+			 * Usefull if you want to animate every element of the menu with some eye-candy effect.
+			 * @param data a json where the padding data is stored accordin to 'type' of the config
+			 * @param element the element which data refers to
+			 */
+			onSingleElement			:	function(data, element){
+				switch(this.settings.type) {
+					case 'padding': 
+						$(element).css({
+							'padding-right'		:		data.paddingLeft, 
+							'padding-left'		:		data.paddingRight
+						});
+						break;
+					case 'left':
+						$(element).css({
+							'left'				:		data.pointer
+						});
+						break;
+					case 'width left':
+						$(element).css({
+							'width'				:		data.pointer
+						});
+						break;
+				}
+			},
+			/**
+			 * This is called when everything is done for the current istance.
+			 * Usefull if you want to animate the whole menu with an effect like fade-in
+			 */
+			onReady					:	function(){return true;}
 		};
-		if(options) $.extend(settings,options);
+		if(options) $.extend(this.settings,options);
+		var me = this;
 		return this.each(function(){
-			var elements = $(this).find(settings.elementsSelector);
+			var that = $(this);
+			console.log(this);
+			var elements = that.find(me.settings.elementsSelector);
 			var elementsWidth = 0;
 			
 			elements.each(function(){
 				elementsWidth += $(this).outerWidth();//outerWidth(true); doesn't work good on ie9 :|
 			});
 			
-			var css = {};
-			
-			switch(settings.type) {
+			switch(me.settings.type) {
 				case 'padding': 
 					var container_space = $(this).outerWidth();
-					var free_space = container_space-settings.containerBorder-elementsWidth;
+					var free_space = container_space-me.settings.containerBorder-elementsWidth;
 					var padding = Math.floor((free_space/elements.size())/2.0);
 					var total_padding = padding*2*elements.size();
 					var delta = free_space-total_padding;
 
 					elements.each(function(key, value){
-						var paddingLeft		= padding+(((--delta)>1)?1:0);
-						var paddingRight	= padding;
-						$(this).css({
-							'padding-right'		:		paddingLeft, 
-							'padding-left'		:		paddingRight
-						});
+						var data = {
+							paddingLeft		:	padding+(((--delta)>1)?1:0),
+							paddingRight	:	padding
+						};
+						if(jQuery.isFunction(me.settings.onSingleElement)){
+							me.settings.onSingleElement.call(me,data,value);
+						}
 					});
 				break;
 				
 				//TODO: need some test here
 				case 'left':
-					var delta = Math.floor(((($(this).outerWidth(true)-settings.containerBorder)-elementsWidth)/elements.size()));
+					var delta = Math.floor(((($(this).outerWidth(true)-me.settings.containerBorder)-elementsWidth)/elements.size()));
 					var pointer = 0;
-					elements.each(function(index){
-						$(this).css({
-							'left'		:		pointer
-						});
+					elements.each(function(key, value){
+						if(jQuery.isFunction(me.settings.onSingleElement)){
+							me.settings.onSingleElement.call(me,{left:pointer},value);
+						}
 						pointer = pointer+delta+$(this).outerWidth(true);
 					});
 				break;
 					
 				case 'width left':
-					var delta = Math.floor(((($(this).outerWidth(true)-settings.containerBorder)-elementsWidth)/elements.size()))-Math.floor(settings.separatorWidth/elements.size());
+					var delta = Math.floor(((($(this).outerWidth(true)-me.settings.containerBorder)-elementsWidth)/elements.size()))-Math.floor(me.settings.separatorWidth/elements.size());
 					var pointer = 0;
-					elements.each(function(index){
+					elements.each(function(key, value){
 						pointer = pointer+$(this).outerWidth(true);
-						$(this).css({
-							'width'		:		pointer
-						});
+						if(jQuery.isFunction(me.settings.onSingleElement)){
+							me.settings.onSingleElement.call(me,{left:pointer},value);
+						}
 						pointer = pointer+delta;
 					});
 				break;
+			}
+			if(jQuery.isFunction(me.settings.onReady)){
+				me.settings.onReady.call(that);
 			}
 			
 			
